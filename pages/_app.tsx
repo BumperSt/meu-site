395012e4d12eb0f5c3cd3735229b4e59bf2782cf
Gabriel Mesquita <bumper@outlook.com.br>
2022-01-18 12:32:40 -0300

    updates Config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { HeadBar } from '@/components/HeadBar/headBar'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return(
     <>
+    <Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
+      <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"/>
+    </Head>
           <HeadBar/>
 
       <Component {...pageProps} />
